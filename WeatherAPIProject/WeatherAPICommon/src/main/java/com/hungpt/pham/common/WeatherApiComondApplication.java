28a954f68be4600ec422952d9875fb6764e9271e
phamt <hungpt11.demt@gmail.com>
2024-12-07 21:20:53 +0700

    Init Project

---
+++ WeatherAPIProject/WeatherAPICommon/src/main/java/com/hungpt/pham/common/WeatherApiComondApplication.java
@@ -0,0 +1,13 @@
+package com.hungpt.pham.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiComondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiComondApplication.class, args);
+    }
+
+}
